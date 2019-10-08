c038908949994ed753e230ac7b4ab25a53ffc8de
marcosecchi <loziosecchi@gmail.com>
2019-10-08 09:44:50 +0200

    Initial commit

---
+++ Assets/Scripts/CloudController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CloudController : MonoBehaviour
+{
+
+    // The speed of the cloud object
+    public float speed;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        // Stores the direction of movement
+        Vector2 direction = Vector2.right;
+
+        // Translates the element by direction, speed and time elapsed since last frame
+        transform.Translate(Time.deltaTime * direction * speed);
+    }
+
+    // When the renderer of the GameObject is no longer visible...
+    void OnBecameInvisible()
+    {
+        // ... set its position on the left of the camera
+        Vector2 resetPosition = new Vector2(-20, transform.position.y);
+        transform.position = resetPosition;
+    }
+}
