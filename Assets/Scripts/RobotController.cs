64cb0915dddc20818912d12cf13ab2ba14054261
Marco Secchi <loziosecchi@gmail.com>
2019-10-14 17:43:02 +0200

    Add robot controller script.

---
+++ Assets/Scripts/RobotController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotController : MonoBehaviour
+{
+    // Stores a reference of the Rigidbody2D component (if any)
+    Rigidbody2D _rigidbody;
+
+    // Stores a reference of the SpriteRenderer component (if any)
+    SpriteRenderer _renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Retrieves the rigidbody component
+        _rigidbody = GetComponent<Rigidbody2D>();
+        // Retrieves the sprite renderer component
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
