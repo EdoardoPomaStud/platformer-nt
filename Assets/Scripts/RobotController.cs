bea3afe8d7dbc0500d5f601b93ba380c4b64315c
marcosecchi <loziosecchi@gmail.com>
2019-10-15 14:14:04 +0200

    Add comments.

--- Assets/Scripts/RobotController.cs
+++ Assets/Scripts/RobotController.cs
@@ -8,35 +8,46 @@ public class RobotController : MonoBehaviour
     // Stores a reference of the SpriteRenderer component (if any)
     SpriteRenderer _renderer;
 
+    // A modifier for the movement
     public float speedMultiplier = 10;
 
+    // A modifier for the speed
     public float jumpMultiplier = 10;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         // Retrieves the rigidbody component
         _rigidbody = GetComponent<Rigidbody2D>();
+        
         // Retrieves the sprite renderer component
         _renderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        // Gets the horizontal thumbstick axis (-1:+1) and stores the
+        // value in a variable
         float hMove = Input.GetAxis("Horizontal");
 
+        // Store in a variable the movement (horizontal) force
         Vector2 moveForce = Vector2.right * hMove * speedMultiplier;
+        
+        // Adds the evaluated force to the rigidbody
         _rigidbody.AddForce(moveForce);
 
-        // Input.GetButtonDown("Jump")
-        
+        // If the jump button has been pressed this frame...
         if(Input.GetButtonDown("Jump"))
         {
+            // Store in a variable the jump (vertical) force
             Vector2 jumpForce = Vector2.up * jumpMultiplier;
+
+            // Adds the evaluated force to the rigidbody
             _rigidbody.AddForce(jumpForce, ForceMode2D.Impulse);
         }
 
+        // Flips the sprite depending on the thumbstick axis
         if(hMove > 0)
         {
             _renderer.flipX = false;
